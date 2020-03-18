3b2d122773de7f06b9e34766f9c60037efc1c3ed
Ayzat Sadykov <ayzat.ziko.93@gmail.com>
2020-03-19 01:40:34 +0300

    leetcode: uncommented test for pascals trangle test

--- leetcode/pascals_triangle_test.go
+++ leetcode/pascals_triangle_test.go
@@ -10,8 +10,8 @@ func TestPascalsTriangle(t *testing.T) {
 		input int
 		want  [][]int
 	}{
-		// {0, nil},
-		// {1, [][]int{[]int{1}}},
+		{0, nil},
+		{1, [][]int{[]int{1}}},
 		{5, [][]int{[]int{1}, []int{1, 1}, []int{1, 2, 1}, []int{1, 3, 3, 1}, []int{1, 4, 6, 4, 1}}},
 	}
 
